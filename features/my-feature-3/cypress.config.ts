50ca48c3c580b1e27b30ae8a25d838e972a7d647
Dan Latimer <daniel.latimer@arcticwolf.com>
2024-03-01 09:56:41 -0400

    add cypress to features

---
+++ features/my-feature-3/cypress.config.ts
@@ -0,0 +1,12 @@
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'cypress',
+      webServerCommands: { default: 'nx run nx-multi-feature:serve-static' },
+    }),
+  },
+});
